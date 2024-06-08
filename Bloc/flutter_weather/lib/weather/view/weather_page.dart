8cd1f3a0e9ee7a6ca1a770233a5a786e5a65ef5b
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-06-08 20:33:14 +0700

    SearchPage

--- Bloc/flutter_weather/lib/weather/view/weather_page.dart
+++ Bloc/flutter_weather/lib/weather/view/weather_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_weather/search/search.dart';
 import 'package:flutter_weather/settings/settings.dart';
 import 'package:flutter_weather/weather/weather.dart';
 import 'package:weather_repository/weather_repository.dart';
@@ -44,7 +45,9 @@ class _WeatherViewState extends State<WeatherView> {
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.search, semanticLabel: 'Search'),
         onPressed: () async {
-          // TODO push to SearchPage
+          final city = await Navigator.of(context).push(SearchPage.route());
+          if (!context.mounted) return;
+          await context.read<WeatherCubit>().fetchWeather(city);
         },
       ),
     );
