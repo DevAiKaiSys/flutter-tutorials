8d5bd22ac66d1a051ac0173484ad1db4223bec87
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-06-08 11:48:43 +0700

    define Ticker

--- Bloc/flutter_timer/lib/timer/bloc/timer_bloc.dart
+++ Bloc/flutter_timer/lib/timer/bloc/timer_bloc.dart
@@ -1,13 +1,21 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_timer/ticker.dart';
 
 part 'timer_event.dart';
 part 'timer_state.dart';
 
 class TimerBloc extends Bloc<TimerEvent, TimerState> {
+  final Ticker _ticker;
   static const int _duration = 60;
 
-  TimerBloc() : super(const TimerInitial(_duration)) {
+  StreamSubscription<int>? _tickerSubscription;
+
+  TimerBloc({required Ticker ticker})
+      : _ticker = ticker,
+        super(const TimerInitial(_duration)) {
     // TODO: implement event handlers
   }
 }
