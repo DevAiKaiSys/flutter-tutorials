544f07270a4cd91ce7ecf5c123230377538df089
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-06-08 17:54:28 +0700

    Get Weather & Barrel File Updates

--- Bloc/flutter_weather/packages/open_meteo_api/lib/src/open_meteo_api_client.dart
+++ Bloc/flutter_weather/packages/open_meteo_api/lib/src/open_meteo_api_client.dart
@@ -10,6 +10,12 @@ class LocationRequestFailure implements Exception {}
 /// Exception thrown when the provided location is not found.
 class LocationNotFoundFailure implements Exception {}
 
+/// Exception thrown when getWeather fails.
+class WeatherRequestFailure implements Exception {}
+
+/// Exception thrown when weather for provided location is not found.
+class WeatherNotFoundFailure implements Exception {}
+
 /// {@template open_meteo_api_client}
 /// Dart API Client which wraps the [Open Meteo API](https://open-meteo.com).
 /// {@endtemplate}
@@ -18,6 +24,7 @@ class OpenMeteoApiClient {
   OpenMeteoApiClient({http.Client? httpClient})
       : _httpClient = httpClient ?? http.Client();
 
+  static const _baseUrlWeather = 'api.open-meteo.com';
   static const _baseUrlGeocoding = 'geocoding-api.open-meteo.com';
 
   final http.Client _httpClient;
@@ -45,5 +52,33 @@ class OpenMeteoApiClient {
     if (results.isEmpty) throw LocationNotFoundFailure();
 
     return Location.fromJson(results.first as Map<String, dynamic>);
+  }
+
+  /// Fetches [Weather] for a given [latitude] and [longitude].
+  Future<Weather> getWeather({
+    required double latitude,
+    required double longitude,
+  }) async {
+    final weatherRequest = Uri.https(_baseUrlWeather, 'v1/forecast', {
+      'latitude': '$latitude',
+      'longitude': '$longitude',
+      'current_weather': 'true',
+    });
+
+    final weatherResponse = await _httpClient.get(weatherRequest);
+
+    if (weatherResponse.statusCode != 200) {
+      throw WeatherRequestFailure();
+    }
+
+    final bodyJson = jsonDecode(weatherResponse.body) as Map<String, dynamic>;
+
+    if (!bodyJson.containsKey('current_weather')) {
+      throw WeatherNotFoundFailure();
+    }
+
+    final weatherJson = bodyJson['current_weather'] as Map<String, dynamic>;
+
+    return Weather.fromJson(weatherJson);
   }
 }
