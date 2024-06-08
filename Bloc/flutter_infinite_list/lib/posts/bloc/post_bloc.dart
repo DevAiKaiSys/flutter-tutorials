e4a2caa473f24bc0c856237edfd64ce6bcca5404
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-06-08 12:51:25 +0700

    implement _fetchPosts

--- Bloc/flutter_infinite_list/lib/posts/bloc/post_bloc.dart
+++ Bloc/flutter_infinite_list/lib/posts/bloc/post_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:bloc/bloc.dart';
 import 'package:bloc_concurrency/bloc_concurrency.dart';
 import 'package:equatable/equatable.dart';
@@ -8,6 +10,7 @@ import 'package:stream_transform/stream_transform.dart';
 part 'post_event.dart';
 part 'post_state.dart';
 
+const _postLimit = 20;
 const throttleDuration = Duration(milliseconds: 100);
 
 EventTransformer<E> throttleDroppable<E>(Duration duration) {
@@ -31,16 +34,14 @@ class PostBloc extends Bloc<PostEvent, PostState> {
     if (state.hasReachedMax) return;
     try {
       if (state.status == PostStatus.initial) {
-        /*final posts = await _fetchPosts();*/
-        const posts = null;
+        final posts = await _fetchPosts();
         return emit(state.copyWith(
           status: PostStatus.success,
           posts: posts,
           hasReachedMax: false,
         ));
       }
-      /*final posts = await _fetchPosts(state.posts.length);*/
-      const posts = null;
+      final posts = await _fetchPosts(state.posts.length);
       emit(posts.isEmpty
           ? state.copyWith(hasReachedMax: true)
           : state.copyWith(
@@ -51,5 +52,27 @@ class PostBloc extends Bloc<PostEvent, PostState> {
     } catch (_) {
       emit(state.copyWith(status: PostStatus.failure));
     }
+  }
+
+  Future<List<Post>> _fetchPosts([int startIndex = 0]) async {
+    final response = await httpClient.get(
+      Uri.https(
+        'jsonplaceholder.typicode.com',
+        '/posts',
+        <String, String>{'_start': '$startIndex', '_limit': '$_postLimit'},
+      ),
+    );
+    if (response.statusCode == 200) {
+      final body = json.decode(response.body) as List;
+      return body.map((dynamic json) {
+        final map = json as Map<String, dynamic>;
+        return Post(
+          id: map['id'] as int,
+          title: map['title'] as String,
+          body: map['body'] as String,
+        );
+      }).toList();
+    }
+    throw Exception('error fetching posts');
   }
 }
