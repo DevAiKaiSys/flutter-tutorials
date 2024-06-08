8ed27bbf0eaadfac1f9d3b5cb7a9deeb9ec0d6bc
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-06-08 14:31:47 +0700

    add RepositoryProvider & BlocProvider

--- Bloc/flutter_login/lib/app.dart
+++ Bloc/flutter_login/lib/app.dart
@@ -1,4 +1,8 @@
+import 'package:authentication_repository/authentication_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_login/authentication/authentication.dart';
+import 'package:user_repository/user_repository.dart';
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -8,8 +12,33 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  late final AuthenticationRepository _authenticationRepository;
+  late final UserRepository _userRepository;
+
+  @override
+  void initState() {
+    super.initState();
+    _authenticationRepository = AuthenticationRepository();
+    _userRepository = UserRepository();
+  }
+
+  @override
+  void dispose() {
+    _authenticationRepository.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return RepositoryProvider.value(
+      value: _authenticationRepository,
+      child: BlocProvider(
+        create: (_) => AuthenticationBloc(
+          authenticationRepository: _authenticationRepository,
+          userRepository: _userRepository,
+        ),
+        child: const Placeholder(),
+      ),
+    );
   }
 }
