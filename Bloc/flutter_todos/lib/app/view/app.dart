5f8dd2e3051f814bcc6c5fc1dd82b0d70c977f5a
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-06-08 23:55:59 +0700

    HomeView

--- Bloc/flutter_todos/lib/app/view/app.dart
+++ Bloc/flutter_todos/lib/app/view/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_todos/home/view/view.dart';
 import 'package:flutter_todos/l10n/l10n.dart';
 import 'package:flutter_todos/theme/theme.dart';
 import 'package:todos_repository/todos_repository.dart';
@@ -27,7 +28,7 @@ class AppView extends StatelessWidget {
       darkTheme: FlutterTodosTheme.dark,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const Placeholder(),
+      home: const HomePage(),
     );
   }
 }
