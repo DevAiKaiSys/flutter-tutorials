e40e5025dead43369f5b54c2b00743c2ba39b79b
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-06-15 08:08:33 +0700

    Finish header layout with widgets

--- ticket_app/lib/resentation_layer/screens/home_screen.dart
+++ ticket_app/lib/resentation_layer/screens/home_screen.dart
@@ -5,6 +5,38 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Column(
+                    children: [
+                      Text("Good morning"),
+                      Text("Book Tickets"),
+                    ],
+                  ),
+                  Container(
+                    color: Colors.red,
+                    width: 100,
+                    height: 70,
+                  ),
+                ],
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Search icon"),
+                  Text("Empty space"),
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
