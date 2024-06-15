52dc155eacefc8f76ecda068a217d546577f6596
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-06-15 08:19:10 +0700

    Style Text() widgets

--- ticket_app/lib/resentation_layer/screens/home_screen.dart
+++ ticket_app/lib/resentation_layer/screens/home_screen.dart
@@ -8,32 +8,49 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: ListView(
         children: [
-          Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Column(
-                    children: [
-                      Text("Good morning"),
-                      Text("Book Tickets"),
-                    ],
-                  ),
-                  Container(
-                    color: Colors.red,
-                    width: 100,
-                    height: 70,
-                  ),
-                ],
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Search icon"),
-                  Text("Empty space"),
-                ],
-              ),
-            ],
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Good morning",
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          "Book Tickets",
+                          style: TextStyle(
+                              fontSize: 26,
+                              fontWeight: FontWeight.w500,
+                              color: Color(0xFF3b3b3b)),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      color: Colors.red,
+                      width: 100,
+                      height: 70,
+                    ),
+                  ],
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Search icon"),
+                    Text("Empty space"),
+                  ],
+                ),
+              ],
+            ),
           )
         ],
       ),
