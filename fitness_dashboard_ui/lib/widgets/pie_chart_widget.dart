e312a0c44b9c9e2309484e16a4e3382a1365d9f1
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-07-07 18:01:42 +0700

    use Stack & Positioned show data in center of pie chart

--- fitness_dashboard_ui/lib/widgets/pie_chart_widget.dart
+++ fitness_dashboard_ui/lib/widgets/pie_chart_widget.dart
@@ -1,3 +1,4 @@
+import 'package:fitness_dashboard_ui/const/constant.dart';
 import 'package:fitness_dashboard_ui/data/pie_chart_data.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
@@ -11,13 +12,35 @@ class Chart extends StatelessWidget {
 
     return SizedBox(
       height: 200,
-      child: PieChart(
-        PieChartData(
-          sectionsSpace: 0,
-          centerSpaceRadius: 70,
-          startDegreeOffset: -90,
-          sections: pieChartData.paiChartSelectionDatas,
-        ),
+      child: Stack(
+        children: [
+          PieChart(
+            PieChartData(
+              sectionsSpace: 0,
+              centerSpaceRadius: 70,
+              startDegreeOffset: -90,
+              sections: pieChartData.paiChartSelectionDatas,
+            ),
+          ),
+          Positioned.fill(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: defaultPadding),
+                Text(
+                  "70%",
+                  style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        height: 0.5,
+                      ),
+                ),
+                const SizedBox(height: 8),
+                const Text("of 100%")
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
