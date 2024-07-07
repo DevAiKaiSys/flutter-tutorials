2c3001e0d6bec5409249bd9d04c78464c5b3774a
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-07-07 17:52:48 +0700

    dashboard add padding and SingleChildScrollView & fix error SingleChildScrollView do not Expanded

--- fitness_dashboard_ui/lib/widgets/dashboard_widget.dart
+++ fitness_dashboard_ui/lib/widgets/dashboard_widget.dart
@@ -9,17 +9,22 @@ class DashboardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        SizedBox(height: 18),
-        HeaderWidget(),
-        SizedBox(height: 18),
-        ActivityDetailsCard(),
-        SizedBox(height: 18),
-        LineChartCard(),
-        SizedBox(height: 18),
-        BarGraphCard(),
-      ],
+    return const SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 18),
+        child: Column(
+          children: [
+            SizedBox(height: 18),
+            HeaderWidget(),
+            SizedBox(height: 18),
+            ActivityDetailsCard(),
+            SizedBox(height: 18),
+            LineChartCard(),
+            SizedBox(height: 18),
+            BarGraphCard(),
+          ],
+        ),
+      ),
     );
   }
 }
