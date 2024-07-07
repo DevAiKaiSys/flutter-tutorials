f82c4dd4ce9de28d9f825f197260c1bafe34520e
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-07-07 14:41:48 +0700

    padding & select menu item

--- fitness_dashboard_ui/lib/widgets/side_menu_widget.dart
+++ fitness_dashboard_ui/lib/widgets/side_menu_widget.dart
@@ -1,3 +1,4 @@
+import 'package:fitness_dashboard_ui/const/constant.dart';
 import 'package:fitness_dashboard_ui/data/side_menu_data.dart';
 import 'package:flutter/material.dart';
 
@@ -9,11 +10,15 @@ class SideMenuWidget extends StatefulWidget {
 }
 
 class _SideMenuWidgetState extends State<SideMenuWidget> {
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     final data = SideMenuData();
 
     return Container(
+      padding: const EdgeInsets.symmetric(vertical: 80, horizontal: 20),
+      color: const Color(0xFF171821),
       child: ListView.builder(
         itemCount: data.menu.length,
         itemBuilder: (context, index) => buildMenuEntry(data, index),
@@ -22,21 +27,40 @@ class _SideMenuWidgetState extends State<SideMenuWidget> {
   }
 
   Widget buildMenuEntry(SideMenuData data, int index) {
-    return Row(
-      children: [
-        Icon(
-          data.menu[index].icon,
-          color: Colors.grey,
+    final isSelected = selectedIndex == index;
+
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 5),
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(6.0),
         ),
-        Text(
-          data.menu[index].title,
-          style: const TextStyle(
-            fontSize: 16,
-            color: Colors.grey,
-            fontWeight: FontWeight.normal,
-          ),
-        )
-      ],
+        color: isSelected ? selectionColor : Colors.transparent,
+      ),
+      child: InkWell(
+        onTap: () => setState(() {
+          selectedIndex = index;
+        }),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 7),
+              child: Icon(
+                data.menu[index].icon,
+                color: isSelected ? Colors.black : Colors.grey,
+              ),
+            ),
+            Text(
+              data.menu[index].title,
+              style: TextStyle(
+                fontSize: 16,
+                color: isSelected ? Colors.black : Colors.grey,
+                fontWeight: isSelected ? FontWeight.w600 : FontWeight.normal,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
